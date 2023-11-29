03a615ecb29343f9e21f5aa8f57f68ef19784354
G1anpierre <fgianpierre@gmail.com>
2023-11-29 09:56:09 +0100

    add suspense to handle the loading time of todos and error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import React, {Suspense} from 'react'
 import {Todos} from './components/Todos'
 import {AddTodoForm} from './components/AddTodoForm'
+import {LoadingTodos} from './components/LoadingTodos'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <AddTodoForm />
-      <Todos />
+      <Suspense fallback={<LoadingTodos />}>
+        <Todos />
+      </Suspense>
     </main>
   )
 }
