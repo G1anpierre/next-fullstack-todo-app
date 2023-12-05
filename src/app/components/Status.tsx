b955b9340a4c2227690cbc79b58033bb72cc70e1
G1anpierre <fgianpierre@gmail.com>
2023-12-05 13:25:30 +0100

    fix import of classNamesLib

--- src/app/components/Status.tsx
+++ src/app/components/Status.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {classNames, formattedStatus, statuses} from '../lib/utils'
+import {classNamesLib, formattedStatus, statuses} from '../lib/utils'
 
 export const Status = ({status}: {status: string}) => {
   return (
     <span
-      className={classNames(
+      className={classNamesLib(
         statuses[status],
         'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset',
       )}
